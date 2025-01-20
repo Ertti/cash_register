bc9a349ffd3e23f827a0092678ea42ef4c4dbe06
TTTTTTT-sys <tomass.tauvens4@gmail.com>
2025-01-20 09:12:05 +0200

    start
    dtsrt

---
+++ 345.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from tkinter import PhotoImage
+root = Tk()
+root.geometry('1600x1000')
+bidl = PhotoImage(file="user-icon-on-transparent-background-free-png.png")
+root.mainloop()
